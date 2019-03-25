0f7219fb6b700c0c99e4521a3b3a473e2ee38abc
Adrian Pucułek <puculeka@gmail.com>
2019-03-25 16:13:07 +0100

    Zmiany w bindach

--- app/src/main/java/pl/jermey/clean_boilerplate/viewmodel/ExampleViewModel.kt
+++ app/src/main/java/pl/jermey/clean_boilerplate/viewmodel/ExampleViewModel.kt
@@ -1,16 +1,26 @@
 package pl.jermey.clean_boilerplate.viewmodel
 
 import androidx.lifecycle.LiveData
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import pl.jermey.clean_boilerplate.viewmodel.ExampleViewModel.ExampleState.*
 import pl.jermey.domain.model.example.Post
 import pl.jermey.domain.usecase.GetExampleDataUseCase
+import java.util.concurrent.TimeUnit
 
 class ExampleViewModel(
-        private val getExampleDataUseCase: GetExampleDataUseCase
+    private val getExampleDataUseCase: GetExampleDataUseCase
 ) : StatefulViewModel<ExampleViewModel.ExampleState, ExampleViewModel.ExampleAction>(Loading) {
 
-    val data: LiveData<String> = state.bind<DataLoaded, String> { state -> state?.data?.toString() }
-    val error: LiveData<String> = state.bind<Error, String> { state -> state?.throwable?.toString() }
+    val data: LiveData<String> = state.bindings {
+        bind<JustString> { state -> state.data }
+        bind<Loading> { "Loading data" }
+        default { "WTF" }
+    }
+
+    val error: LiveData<String> = state.bind<Error, String> { state -> state?.throwable.toString() }
     val loading: LiveData<Boolean> = state.bind<Loading, Boolean> { state -> state != null }
 
     override fun dispatchAction(action: ExampleAction) {
@@ -21,18 +31,31 @@ class ExampleViewModel(
     }
 
     private fun postData(data: List<Post>) {
-        
+
     }
 
     private fun getData() = launch {
+        Observable.just("Hello")
+            .delay(2, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onError = {
+                    state.postValue(Error(it))
+                },
+                onNext = {
+                    state.postValue(JustString(it))
+                }
+            )
         getExampleDataUseCase.execute().subscribe(
-                { data -> state.postValue(DataLoaded(data)) },
-                { error -> state.postValue(Error(error)) }
+            { data -> state.postValue(DataLoaded(data)) },
+            { error -> state.postValue(Error(error)) }
         )
     }
 
     sealed class ExampleState : State {
         object Loading : ExampleState()
+        data class JustString(val data: String) : ExampleState()
         data class DataLoaded(val data: List<Post>) : ExampleState()
         data class Error(val throwable: Throwable) : ExampleState()
     }
--- app/src/main/java/pl/jermey/clean_boilerplate/viewmodel/StatefulViewModel.kt
+++ app/src/main/java/pl/jermey/clean_boilerplate/viewmodel/StatefulViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import kotlin.reflect.KClass
 
 interface State
 interface Action
@@ -37,11 +38,43 @@ abstract class StatefulViewModel<STATE : State, ACTION : Action>(initialState: S
         disposables.clear()
     }
 
-    @Suppress("UNCHECKED_CAST")
-    inline fun <reified STATE : State, VALUE> MutableLiveData<*>.bind(noinline transformer: (state: STATE?) -> VALUE?): LiveData<VALUE> {
-        return Transformations.map<Any, VALUE>(this as LiveData<Any>) {
-            transformer(if (value is STATE) value as STATE else null)
+    inline fun <reified STATE1 : STATE, VALUE> MutableLiveData<STATE>.bind(
+        noinline transformer: (state: STATE1?) -> VALUE?
+    ): LiveData<VALUE> {
+        return Transformations.map<STATE, VALUE>(this as LiveData<STATE>) {
+            transformer(if (value is STATE1) value as STATE1 else null)
         }
     }
 
+    fun <VALUE> MutableLiveData<STATE>.bindings(block: Binder<STATE, VALUE>.() -> FullBuilder<STATE, VALUE>) =
+        Binder<STATE, VALUE>(this).let(block).build()
+
+    inner class Binder<S : STATE, VALUE>(private val liveData: LiveData<S>) {
+
+        val transformers: MutableMap<KClass<*>, Function1<*, VALUE>> = mutableMapOf()
+
+        inline fun <reified STATE1 : S> bind(noinline transformer: Function1<STATE1, VALUE>) {
+            if (transformers[STATE1::class] != null) throw RuntimeException("Should not override already present key")
+            transformers[STATE1::class] = transformer
+        }
+
+        fun default(transformer: Function0<VALUE?>): FullBuilder<S, VALUE> =
+            FullBuilder(liveData, transformers, transformer)
+    }
+
+    inner class FullBuilder<S : STATE, VALUE>(
+        private val liveData: LiveData<S>,
+        private val transformers: MutableMap<KClass<*>, Function1<*, VALUE>>,
+        private val defaultTransformer: Function0<VALUE?>
+    ) {
+
+        @Suppress("UNCHECKED_CAST")
+        fun build(): LiveData<VALUE> = Transformations
+            .map<S, VALUE>(liveData) { s ->
+                val transformer = transformers.getOrElse(s::class) { null } as? Function1<S, VALUE>
+                transformer
+                    ?.invoke(s)
+                    ?: defaultTransformer.invoke()
+            }
+    }
 }
